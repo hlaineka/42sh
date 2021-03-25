7f2b8337e72bb009d8b58f99e4be8c962697b00e
hhuhtane <hhuhtane@student.hive.fi>
2021-03-25 20:24:22 +0200

    fix escapechar to work in input

--- srcs/input/ft_is_quote_open.c
+++ srcs/input/ft_is_quote_open.c
@@ -6,7 +6,7 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 10:06:04 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/03/22 10:06:34 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/03/25 20:22:24 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int				ft_is_quote_open(int quote, char *str)
 	{
 		if (!quote && ft_strchr("'\"\\`", *str))
 			quote = *str;
-		else if ((quote == '\\') || (quote == *str))
+		else if ((quote == '\\' && *str != '\n') || (quote == *str))
 			quote = '\0';
 		str++;
 	}
