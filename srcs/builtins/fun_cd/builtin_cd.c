b314f50ad518ce32a8e4524a9620d25b0713e964
hhuhtane <hhuhtane@student.hive.fi>
2021-05-02 13:27:45 +0300

    norminette builtin_cd.c

--- srcs/builtins/fun_cd/builtin_cd.c
+++ srcs/builtins/fun_cd/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 14:22:43 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/05/02 13:03:19 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/05/02 13:26:58 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 //int	builtin_cd(int argc, char **argv, char **envp)
 
+void	get_cwd_and_set_env(char *set_env, char **envp)
+{
+	char		cwd[1024];
+
+	getcwd(cwd, 1024);
+	ft_setenv(set_env, cwd, 1, envp);
+}
+
 void	builtin_cd(void *proc)
 {
-	t_process	*process;
+	char		path[1024];
 	int			argc;
 	char		**argv;
 	char		**envp;
-	process = proc;
-	envp = process->envp;
-	argv = process->argv;
-	argc = process->argc;
 
-	char	path[1024];
-	char	cwd[1024];
-
+	envp = ((t_process *)proc)->envp;
+	argv = ((t_process *)proc)->argv;
+	argc = ((t_process *)proc)->argc;
 	if (argc > 2)
-	{
-		err_builtin(E_TOO_MANY_ARGS, argv[0], NULL);
-		return ;
-//		return (err_builtin(E_TOO_MANY_ARGS, argv[0], NULL));
-	}
+		return ((void)err_builtin(E_TOO_MANY_ARGS, argv[0], NULL));
 	if (argc == 2 && !ft_strcmp(argv[1], "-") && ft_getenv("OLDPWD", envp))
 		ft_strcpy(path, ft_getenv("OLDPWD", envp));
 	else if (argc == 2)
@@ -41,23 +41,11 @@ void	builtin_cd(void *proc)
 	else if (ft_getenv("HOME", envp))
 		ft_strcpy(path, ft_getenv("HOME", envp));
 	else
-	{
-		err_builtin(E_HOME_NOT_SET, argv[0], NULL);
-		return ;
-//		return (err_builtin(E_HOME_NOT_SET, argv[0], NULL));
-	}
+		return ((void)err_builtin(E_HOME_NOT_SET, argv[0], NULL));
 	if (is_valid_folder(path, "cd") != 0)
 		return ;
-//		return (-1);
-	getcwd(cwd, 1024);
-	ft_setenv("OLDPWD", cwd, 1, envp);
+	get_cwd_and_set_env("OLDPWD", envp);
 	if (chdir(path) == -1)
-	{
-		err_builtin(E_CD_CHANGE_FAIL, "cd", path);
-		return ;
-//		return (err_builtin(E_CD_CHANGE_FAIL, "cd", path));
-	}
-	getcwd(cwd, 1024);
-	ft_setenv("PWD", cwd, 1, envp);
-//	return (0);
+		return ((void)err_builtin(E_CD_CHANGE_FAIL, "cd", path));
+	get_cwd_and_set_env("PWD", envp);
 }
