784041b2ebf3beff3945bd0ff0284fbbd0121b01
hhuhtane <hhuhtane@student.hive.fi>
2021-05-08 17:40:26 +0300

    clean error message in builin_echo

--- srcs/builtins/fun_echo/builtin_echo.c
+++ srcs/builtins/fun_echo/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:57:52 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/05/08 17:18:00 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/05/08 17:31:02 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 static int	validate_stdout(t_process *process)
 {
-	if (check_fd(STDOUT_FILENO, 1) == -1)
+	if (check_fd(STDOUT_FILENO, 0) == -1)
 	{
+		ft_printf_fd(STDERR_FILENO, "21sh: echo: ");
+		ft_printf_fd(STDERR_FILENO, "write error: Bad file descriptor\n");
 		process->completed = 1;
 		process->status = 1;
 		return (1);
