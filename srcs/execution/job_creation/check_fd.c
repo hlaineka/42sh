22d981669b1c3daf4120e4314954740d751dc381
hhuhtane <hhuhtane@student.hive.fi>
2021-05-08 17:39:30 +0300

    change to use stack memory in check_fd.c

--- srcs/execution/job_creation/check_fd.c
+++ srcs/execution/job_creation/check_fd.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/02 15:11:21 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/05/03 16:10:45 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/05/08 17:38:05 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,19 @@
 
 int	check_fd(int fd, int errormessage)
 {
-	struct stat	*buf;
+	struct stat	buf;
 	int			returnable;
 
-	buf = NULL;
-	buf = malloc(sizeof(struct stat));
 	returnable = 0;
-	if (-1 == fstat(fd, buf))
+	if (-1 == fstat(fd, &buf))
 	{
 		if (errormessage == 1)
 			ft_printf_fd(STDERR_FILENO, "Bad file descriptor\n");
-		ft_free(buf);
 		return (-1);
 	}
-	if (buf->st_mode & S_IRUSR)
+	if (buf.st_mode & S_IRUSR)
 		returnable = 3;
-	if (buf->st_mode & S_IWUSR)
+	if (buf.st_mode & S_IWUSR)
 		returnable++;
-	ft_free(buf);
 	return (returnable);
 }
