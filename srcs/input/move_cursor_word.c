e35abdcb7600206086907bf2a785bd09543a257b
hhuhtane <hhuhtane@student.hive.fi>
2021-05-08 20:52:44 +0300

    fix move_cursor_word to work with history inputs that have newlines

--- srcs/input/move_cursor_word.c
+++ srcs/input/move_cursor_word.c
@@ -6,7 +6,7 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 11:51:38 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/03/26 11:59:37 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/05/08 20:51:02 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,10 @@ void	move_prev_word(t_input *input, t_term *term)
 		return ;
 	i = len - 1;
 	while (i >= 0 && input->ls[i]
-		&& (input->ls[i] == ' ' || input->ls[i] == 13))
+		&& (input->ls[i] == ' ' || input->ls[i] == 13 || input->ls[i] == 10))
 		i--;
 	while (i >= 0 && input->ls[i]
-		&& (input->ls[i] != ' ' && input->ls[i] != 13))
+		&& (input->ls[i] != ' ' && input->ls[i] != 13 && input->ls[i] != 10))
 		i--;
 	i = len - i - 1;
 	move_nchars_from_s1_to_s2(input->ls, input->rrs, input->ls_size, i);
@@ -58,10 +58,10 @@ void	move_next_word(t_input *input, t_term *term)
 		return ;
 	i = len - 1;
 	while (i > 0 && input->rrs[i] && input->rrs[i] != ' '
-		&& input->rrs[i] != 13)
+		&& input->rrs[i] != 13 && input->rrs[i] != 10)
 		i--;
 	while (i > 0 && input->rrs[i] && (input->rrs[i] == ' '
-			|| input->rrs[i] == 13))
+			|| input->rrs[i] == 13 || input->rrs[i] == 10))
 		i--;
 	i = len - i - 1;
 	move_nchars_from_s1_to_s2(input->rrs, input->ls, input->ls_size, i);
