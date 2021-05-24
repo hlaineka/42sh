fd965398467b046128e5047832dd1a0ff0301e89
hhuhtane <hhuhtane@student.hive.fi>
2021-05-24 20:02:47 +0300

    fix input quote check with last char being escape char inside double quotes

--- srcs/input/ft_is_quote_open.c
+++ srcs/input/ft_is_quote_open.c
@@ -6,11 +6,29 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 10:06:04 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/05/22 16:06:55 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/05/24 20:01:11 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
+
+static void	check_character(char c, int *quote, int *temp_quote)
+{
+	if (!(*quote) && ft_strchr("'\"\\`", c))
+		*quote = c;
+	else if (*quote == '"' && c == '\\')
+	{
+		*temp_quote = '"';
+		*quote = '\\';
+	}
+	else if ((*quote == '\\' && c != '\n') || (*quote == c))
+		*quote = 0;
+	if (*quote != '\\' && *temp_quote)
+	{
+		*quote = *temp_quote;
+		*temp_quote = 0;
+	}
+}
 
 int	ft_is_quote_open(int quote, char *str)
 {
@@ -21,21 +39,10 @@ int	ft_is_quote_open(int quote, char *str)
 		return (PROMPT_NORMAL);
 	while (*str)
 	{
-		if (!quote && ft_strchr("'\"\\`", *str))
-			quote = *str;
-		else if (quote == '"' && *str == '\\')
-		{
-			temp_quote = '"';
-			quote = '\\';
-		}
-		else if ((quote == '\\' && *str != '\n') || (quote == *str))
-			quote = 0;
-		if (quote != '\\' && temp_quote)
-		{
-			quote = temp_quote;
-			temp_quote = 0;
-		}
+		check_character(*str, &quote, &temp_quote);
 		str++;
 	}
+	if (temp_quote)
+		quote = temp_quote;
 	return (quote);
 }
