a30ff867a6242fa6de366c6a1d6fbe4f5f9cd11d
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-05-27 16:28:09 +0300

    test prints off

--- srcs/parser/tokenization/lexer.c
+++ srcs/parser/tokenization/lexer.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 15:32:48 by helvi             #+#    #+#             */
-/*   Updated: 2021/05/27 12:52:12 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/05/27 16:19:27 by hlaineka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,13 @@ static char	*handle_backslash_remove(char *str, int i)
 		new[w] = str[w];
 		w++;
 	}
-	ft_printf("%s\n", new);
 	new[w++] = 39;
-	ft_printf("%s\n", new);
 	i++;
 	new[w++] = str[i++];
-	ft_printf("%s\n", new);
 	new[w++] = 39;
-	ft_printf("%s\n", new);
 	while(w < len)
 		new[w++] = str[i++];
 	new[w] = '\0';
-	ft_printf("%s\n", new);
 	return (new);
 }
 
