68081ce989e6cfd47696a5f345723fa805e8f634
hhuhtane <hhuhtane@student.hive.fi>
2021-07-22 20:43:21 +0300

    second draft of builtin type

--- srcs/builtins/fun_type/builtin_type.c
+++ srcs/builtins/fun_type/builtin_type.c
@@ -6,17 +6,26 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:57:52 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/07/22 20:24:08 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/07/22 20:42:24 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int		print_type(char *cmd, t_term *term)
+int		print_type(char *cmd, t_process *p, t_term *term)
 {
+	char		buf[1024];
+	char		*path;
+
 	(void)term;
+	path = ft_getenv("PATH", p->envp);
 	if (is_builtin_type(cmd))
 		ft_printf("%s is a shell builtin\n", cmd);
+	else
+	{
+		find_path(cmd, path, buf);
+		ft_printf("%s is a %s\n", cmd, buf);
+	}
 	return (0);
 }
 
@@ -36,7 +45,7 @@ void	builtin_type(void *proc)
 	argv++;
 	while (*argv)
 	{
-		print_type(*argv, p->term_ptr);
+		print_type(*argv, p, p->term_ptr);
 		argv++;
 	}
 
