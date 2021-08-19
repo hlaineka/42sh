6cb5de90e89d7c9251f70668eab86e4001e021c8
hlaineka <helvi.lainekallio@gmail.com>
2021-08-19 20:29:09 +0300

    now only the first words are considered assignment words

--- srcs/parser/tokenization/word_assignment_marking.c
+++ srcs/parser/tokenization/word_assignment_marking.c
@@ -6,45 +6,84 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 14:39:32 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/08/01 11:21:54 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/08/19 20:27:58 by hlaineka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "libft.h"
 #include "builtins.h"
+#include "includes.h"
+
+int	check_assignment_word (t_token *tkn)
+{
+	char	name[STR_LENGTH];
+	int		i;
+	int		returnable;
+	int 	i_equal_sign;
+
+	returnable = 1;
+	ft_bzero(name, STR_LENGTH);
+	i = 0;
+	i_equal_sign = ft_str_find_c(tkn->value, '=');
+	ft_printf("%i\n", i_equal_sign);
+	if (i_equal_sign > 0 && tkn->quotes[i_equal_sign] == 0)
+	{
+		while (i < i_equal_sign)
+		{
+			ft_printf("%s\n", name);
+			name[i] = tkn->value[i];
+			i++;
+		}
+		name[i] = '\0';
+		if (ft_is_name(name))
+			tkn->maintoken = tkn_assignment_word;
+		else
+			returnable = 0;
+	}
+	else
+		returnable = 0;
+	ft_printf("%i\n", returnable);
+	ft_printf("%s\n", name);
+	return (returnable);
+}
+
+int	is_splitting_operator(t_token *tkn)
+{
+	if (tkn->maintoken == tkn_and || tkn->maintoken == tkn_lpar
+	|| tkn->maintoken == tkn_rpar || tkn->maintoken == tkn_semi
+	|| tkn->maintoken == tkn_nl || tkn->maintoken == tkn_pipe
+	|| tkn->maintoken == tkn_and_if || tkn->maintoken == tkn_or_if
+	|| tkn->maintoken == tkn_lbrace || tkn->maintoken == tkn_rbrace
+	|| tkn->maintoken == tkn_lesslpar || tkn->maintoken == tkn_greatlpar
+	|| tkn->maintoken == tkn_dollarlpar)
+		return (1);
+	return (0);
+}
 
 t_token	*word_assignment_marking(t_token *first)
 {
 	t_token *temp;
-	int		i_equal_sign;
-	int		i;
-	char	*name;
+	int		is_first_command_word;
 	int		only_assignments;
 
 	temp = first;
 	only_assignments = 1;
+	is_first_command_word = 1;
 	while (temp)
 	{
-		i_equal_sign = ft_str_find_c(temp->value, '=');
-		if (i_equal_sign > 0 && temp->quotes[i_equal_sign] == 0)
+		if (is_splitting_operator(temp))
 		{
-			name = malloc(i_equal_sign + 1);
-			i = 0;
-			while (i < i_equal_sign)
+			is_first_command_word = 1;
+			continue;
+		}
+		if (is_first_command_word)
+		{
+			if (check_assignment_word(temp) == 0)
 			{
-				if (temp->quotes[i] != 0)
-				{
-					ft_free(name);
-					break;
-				}
-				name[i] = temp->value[i];
-				i++;
+				only_assignments = 0;
+				is_first_command_word = 0;
 			}
-			name[i] = '\0';
-			if (ft_is_name(name))
-				temp->maintoken = tkn_assignment_word;
-			ft_free(name);
 		}
 		else
 			only_assignments = 0;
