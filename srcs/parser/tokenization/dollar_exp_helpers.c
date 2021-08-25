c6ea090b10c5152512a5bc2c8ef7449ab10a76a8
hhuhtane <hhuhtane@student.hive.fi>
2021-08-25 21:51:21 +0300

    fix norme

--- srcs/parser/tokenization/dollar_exp_helpers.c
+++ srcs/parser/tokenization/dollar_exp_helpers.c
@@ -6,7 +6,7 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 13:16:33 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/08/25 21:15:59 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/08/25 21:50:43 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,84 +15,87 @@
 #include "builtins.h"
 
 /*
-** ${#parameter}
-**   String Length. The length in characters of the value of parameter shall
-**   be substituted. If parameter is '*' or '@', the result of the expansion
-**   is unspecified. If parameter is unset and set -u is in effect, the
-**   expansion shall fail.
+** ${variable}
+**    If you write braces around the variable name, then it is completely
+**    unambiguous where the variable name ends. You can concatenate additional
+**    letters onto the end of the variable value by writing them immediately
+**    after the close brace. For example, ‘${foo}s’ expands into ‘tractors’.
 **
-** The following four varieties of parameter expansion provide for substring
-** processing. In each case, pattern matching notation (see Pattern Matching
-** Notation), rather than regular expression notation, shall be used to
-** evaluate the patterns. If parameter is '#', '*', or '@', the result of the
-** expansion is unspecified. If parameter is unset and set -u is in effect,
-** the expansion shall fail. Enclosing the full parameter expansion string in
-** double-quotes shall not cause the following four varieties of pattern
-** characters to be quoted, whereas quoting characters within the braces
-** shall have this effect. In each variety, if word is omitted, the empty
-** pattern shall be used.
+** You can also use various constructs to modify the value that is substituted,
+** or test it in various ways.
 **
-** ${parameter%[word]}
-Remove Smallest Suffix Pattern. The word shall be expanded to produce a pattern. The parameter expansion shall then result in parameter, with the smallest portion of the suffix matched by the pattern deleted. If present, word shall not begin with an unquoted '%'.
-${parameter%%[word]}
-Remove Largest Suffix Pattern. The word shall be expanded to produce a pattern. The parameter expansion shall then result in parameter, with the largest portion of the suffix matched by the pattern deleted.
-${parameter#[word]}
-Remove Smallest Prefix Pattern. The word shall be expanded to produce a pattern. The parameter expansion shall then result in parameter, with the smallest portion of the prefix matched by the pattern deleted. If present, word shall not begin with an unquoted '#'.
-${parameter##[word]}
-Remove Largest Prefix Pattern. The word shall be expanded to produce a pattern. The parameter expansion shall then result in parameter, with the largest portion of the prefix matched by the pattern deleted.
+** ${variable:-default}
+**    Substitute the value of variable, but if that is empty or undefined, use
+**     default instead.
+**
+** ${variable:=default}
+**    Substitute the value of variable, but if that is empty or undefined, use
+**    default instead and set the variable to default.
+**
+** ${variable:?message}
+**    If variable is defined and not empty, substitute its value.
+**
+** Otherwise, print message as an error message on the standard error
+** stream, and consider word expansion a failure.
+**
+** ${variable:+replacement}
+**    Substitute replacement, but only if variable is defined and nonempty.
+**    Otherwise, substitute nothing for this construct.
+**
+** ${#variable}
+**    Substitute a numeral which expresses in base ten the number of characters
+**    in the value of variable. ‘${#foo}’ stands for ‘7’, because ‘tractor’ is
+**    seven characters.
+**
+** These variants of variable substitution let you remove part of the
+** variable’s value before substituting it. The prefix and suffix are not
+** mere strings; they are wildcard patterns, just like the patterns that
+** you use to match multiple file names. But in this context, they match
+** against parts of the variable value rather than against file names.
+**
+** ${variable%%suffix}
+**    Substitute the value of variable, but first discard from that variable
+**    any portion at the end that matches the pattern suffix.
+**
+**    If there is more than one alternative for how to match against suffix,
+**    this construct uses the longest possible match.
+**
+**    Thus, ‘${foo%%r*}’ substitutes ‘t’, because the largest match for ‘r*’ at
+**    the end of ‘tractor’ is ‘ractor’.
+**
+** ${variable%suffix}
+**    Substitute the value of variable, but first discard from that variable
+**    any portion at the end that matches the pattern suffix.
+**
+**    If there is more than one alternative for how to match against suffix,
+**    this construct uses the shortest possible alternative.
+**
+**    Thus, ‘${foo%r*}’ substitutes ‘tracto’, because the shortest match for
+**    ‘r*’ at the end of ‘tractor’ is just ‘r’.
+**
+** ${variable##prefix}
+**    Substitute the value of variable, but first discard from that variable
+**    any portion at the beginning that matches the pattern prefix.
+**
+**    If there is more than one alternative for how to match against prefix,
+**    this construct uses the longest possible match.
+**
+**    Thus, ‘${foo##*t}’ substitutes ‘or’, because the largest match for ‘*t’
+**    at the beginning of ‘tractor’ is ‘tract’.
+**
+** ${variable#prefix}
+**    Substitute the value of variable, but first discard from that variable
+**    any portion at the beginning that matches the pattern prefix.
+**
+**    If there is more than one alternative for how to match against prefix,
+**    this construct uses the shortest possible alternative.
+**
+**    Thus, ‘${foo#*t}’ substitutes ‘ractor’, because the shortest match for
+**    ‘*t’ at the beginning of ‘tractor’ is just ‘t’.
 */
-
-int	get_subst_option(char *param, char **colon)
-{
-	int		options;
-	int		len;
-
-	options = 0;
-	len = ft_strlen(param);
-	*colon = ft_strchr(param, ':');
-	if (param[0] == '#')
-		options = PRE_HASH;
-	else if (param[len - 1] == '%')
-	{
-		if (param[len - 2] == '%')
-		{
-			options = DPROCENT;
-		}
-		else
-			options = PROCENT;
-	}
-	else if (*colon)
-	{
-		if (colon[0][1] == '-')
-		{
-			options = COLON_MINUS;
-			return (COLON_MINUS);
-		}
-		else if (colon[0][1] == '=')
-			options = COLON_EQUAL;
-		else if (colon[0][1] == '?')
-			options = COLON_QUESTION;
-		else if (colon[0][1] == '+')
-			options = COLON_PLUS;
-	}
-	else if (param[len - 1] == '#')
-	{
-		if (param[len - 2] == '#')
-			options = POST_HASH;
-		else
-			options = POST_DHASH;
-	}
-	else
-		options = 0;
-	return (options);
-}
 
 char	*get_param_str(char *param, t_term *term)
 {
-//	char	*word;
-	char	*whitespace;
-	int		option;
-
 	if (param[0] == '#')
 		return (get_param_length(param + 1, term));
 	if (ft_strstr(param, ":-"))
@@ -109,14 +112,5 @@ char	*get_param_str(char *param, t_term *term)
 		return (substitute_var_without_prefix(param, term));
 	if (ft_strstr(param, "%"))
 		return (substitute_var_without_suffix(param, term));
-	option = get_subst_option(param, &whitespace);
-	if (!option)
-		return (ft_strdup(ft_getenv(param, term->envp)));
-	if (whitespace && whitespace[0])
-	{
-		whitespace[0] = '\0';
-		whitespace[1] = '\0';
-		return (get_param_colon_word(param, whitespace + 2, option, term));
-	}
-	return (ft_strdup("VAKIO TESTI MUU"));
+	return (ft_strdup(ft_getenv(param, term->envp)));
 }
