50ff708d0534c43237860d5571d7d78c413d7d3e
hhuhtane <hhuhtane@student.hive.fi>
2021-08-25 22:08:01 +0300

    remove debug prints

--- srcs/parser/tokenization/dollar_expansion.c
+++ srcs/parser/tokenization/dollar_expansion.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 21:44:33 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/08/25 22:06:07 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/08/25 22:07:31 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,8 @@ static int	substitute_dollar(t_token *tkn, t_term *term, int start, int end)
 	add_quotearray(tkn);
 //	ft_free(parameter);
 //	ft_free(substitution);
-	ft_printf("%s: 1\n", __FUNCTION__);
 	ft_memdel((void **)&parameter);
-	ft_printf("%s: 2\n", __FUNCTION__);
 	ft_memdel((void **)&substitution);
-	ft_printf("%s: 3\n", __FUNCTION__);
 	return (0);
 }
 
