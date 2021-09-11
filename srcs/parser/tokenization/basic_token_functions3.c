1c6a3e268413d12bd260d185fd64b0cc09aa7e00
hlaineka <helvi.lainekallio@gmail.com>
2021-09-11 13:10:12 +0300

    redirection operator handling updated after parser changes

--- srcs/parser/tokenization/basic_token_functions3.c
+++ srcs/parser/tokenization/basic_token_functions3.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 17:58:27 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/09/11 12:02:33 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/09/11 12:57:58 by hlaineka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,6 @@ int	handle_operator_token(char *str, char *source, int *i, int *maintoken)
 		else
 			*maintoken = tkn_redirop;
 	}
-	else if (source[*i] == '&' || source[*i] == '|'
-		|| source[*i] == ';')
-		*maintoken = tkn_operator;
 	else if ((*maintoken != tkn_operator || *maintoken != tkn_redirop)
 		&& str[0])
 		return (0);
@@ -66,7 +63,7 @@ int	handle_word_token(char *str, char *source, int *i, int *maintoken)
 	int	w;
 
 	if (*maintoken != tkn_word)
-		return (1);
+		return (0);
 	w = ft_strlen(str);
 	str[w] = source[*i];
 	return (1);
