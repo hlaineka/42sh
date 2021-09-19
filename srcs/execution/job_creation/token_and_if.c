8ce0edabf4f16e8dae43da148b4ebf0e3603d885
hlaineka <helvi.lainekallio@gmail.com>
2021-09-19 19:58:07 +0300

    assignment word with and_if gub fixed

--- srcs/execution/job_creation/token_and_if.c
+++ srcs/execution/job_creation/token_and_if.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 19:39:06 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/09/18 19:37:57 by hlaineka         ###   ########.fr       */
+/*   Updated: 2021/09/19 19:57:28 by hlaineka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,16 @@ t_job	*token_and_if(t_job *job, t_term *term, t_node *current)
 		left = tree_traversal(NULL, current->left, term);
 	if (left && current->left->operation != tkn_semi
 		&& current->left->operation != tkn_pipe
-		&& current->left->operation != tkn_and 
+		&& current->left->operation != tkn_and
 		&& current->left->operation != tkn_and_if
-		&& current->left->operation != tkn_or_if
-		&& current->left->operation != tkn_assignment)
+		&& current->left->operation != tkn_or_if)
 	{
 		left->next = term->jobs->next;
 		term->jobs->next = left;
 		if (left->first_process->pid == 0)
 			left->first_process->status = simple_command(left->first_process, left, term);
 	}
+	ft_printf("%i\n", left->first_process->status);
 	if (left->first_process->status == 0)
 		get_right(current, term);
 	return (term->jobs->next);
