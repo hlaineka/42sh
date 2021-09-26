81f1d93384522c5a193a85b78a15a1fd218a3ac4
hhuhtane <hhuhtane@student.hive.fi>
2021-09-26 10:46:41 +0300

    fix cd norm

--- srcs/builtins/fun_cd/cd_remove_dots.c
+++ srcs/builtins/fun_cd/cd_remove_dots.c
@@ -6,7 +6,7 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 19:47:03 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/09/26 10:22:06 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/09/26 10:46:03 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,34 @@ void	cd_lst_delete(void *data, size_t size)
 	free(data);
 }
 
+static int	cd_remove_dotdot(t_list *start, t_list **prev, t_list **lst)
+{
+	t_list	*prev_ptr;
+	t_list	*lst_ptr;
+
+	prev_ptr = *prev;
+	lst_ptr = *lst;
+	if (start == prev_ptr)
+	{
+		prev_ptr->next = lst_ptr->next;
+		ft_lstdelone(&lst_ptr, &cd_lst_delete);
+		lst_ptr = prev_ptr->next;
+		return (1);
+	}
+	while (start->next != prev_ptr)
+		start = start->next;
+	start->next = lst_ptr->next;
+	ft_lstdelone(&lst_ptr, &cd_lst_delete);
+	ft_lstdelone(&prev_ptr, &cd_lst_delete);
+	*lst = start->next;
+	*prev = start;
+	return (0);
+}
+
 void	cd_remove_dots(t_list *lst)
 {
 	t_list	*start;
 	t_list	*prev;
-	t_list	*temp;
 
 	start = lst;
 	prev = lst;
@@ -34,28 +57,13 @@ void	cd_remove_dots(t_list *lst)
 			prev->next = lst->next;
 			ft_lstdelone(&lst, &cd_lst_delete);
 			lst = prev->next;
-			continue ;
 		}
 		else if (!ft_strcmp(lst->content, ".."))
+			cd_remove_dotdot(start, &prev, &lst);
+		else
 		{
-			temp = start;
-			if (temp == prev)
-			{
-				prev->next = lst->next;
-				ft_lstdelone(&lst, &cd_lst_delete);
-				lst = prev->next;
-				continue ;
-			}
-			while (temp->next != prev)
-				temp = temp->next;
-			temp->next = lst->next;
-			ft_lstdelone(&lst, &cd_lst_delete);
-			ft_lstdelone(&prev, &cd_lst_delete);
-			lst = temp->next;
-			prev = temp;
-			continue ;
+			lst = lst->next;
+			prev = prev->next;
 		}
-		lst = lst->next;
-		prev = prev->next;
 	}
 }
