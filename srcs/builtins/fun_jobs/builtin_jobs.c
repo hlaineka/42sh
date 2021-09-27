cbc49b30643ebb77260967fe2386cdb0ee356180
hhuhtane <hhuhtane@student.hive.fi>
2021-09-27 19:16:52 +0300

    fix options fail status in jobs

--- srcs/builtins/fun_jobs/builtin_jobs.c
+++ srcs/builtins/fun_jobs/builtin_jobs.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:57:52 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/07/11 14:54:16 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/09/27 19:15:31 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ static void	list_active_jobs(int opt, char *jobspec_str, int lst)
 
 void	builtin_jobs(void *proc)
 {
-	t_term		*term;
 	t_job		*jobs;
 	t_process	*process;
 	int			i;
@@ -53,20 +52,19 @@ void	builtin_jobs(void *proc)
 	i = 0;
 	options = 0;
 	process = proc;
-	term = g_term;
-	jobs = term->jobs->next;
-	update_status(term);
-	if (!jobs->next)
+	jobs = g_term->jobs->next;
+	update_status(g_term);
+	i = get_argv_options(process->argv, &options);
+	if (options != 0
+		&& (options != (1 << L_FLAG))
+		&& (options != (1 << P_FLAG)))
+		process->status = err_builtin(E_ILLEGAL_OPTION, "jobs", NULL);
+	if (!jobs->next || process->status)
 		return ;
 	if (process->argc == 1)
 	{
 		list_active_jobs(0, NULL, get_next_job_pgid(jobs));
 		return ;
 	}
-	i = get_argv_options(process->argv, &options);
-	if (options != 0
-		&& (options != (1 << L_FLAG))
-		&& (options != (1 << P_FLAG)))
-		return ((void)err_builtin(E_ILLEGAL_OPTION, "jobs", NULL));
 	list_active_jobs(options, process->argv[i], get_next_job_pgid(jobs));
 }
