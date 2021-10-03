b768f0dbc090af9a5e9d69453986c14e58b0ae3d
hhuhtane <hhuhtane@student.hive.fi>
2021-10-03 21:23:43 +0300

    norm fixes

--- srcs/parser/tokenization/lexer_functions.c
+++ srcs/parser/tokenization/lexer_functions.c
@@ -6,7 +6,7 @@
 /*   By: hlaineka <hlaineka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 12:26:22 by hlaineka          #+#    #+#             */
-/*   Updated: 2021/10/03 20:38:45 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/10/03 21:20:37 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,39 +107,37 @@ static t_token	*run_or_if_command(t_token **command_first, t_term *term,
 	return (returnable);
 }
 
+static void	callback_and_set(t_token **temp, t_token **command_first,
+	t_term *term, t_token *(*callback)(t_token **, t_term *, t_token **))
+{
+	*command_first = callback(command_first, term, temp);
+	*temp = *command_first;
+}
+
 t_token	*check_semicolon(t_token *first, t_term *term)
 {
 	t_token	*temp;
-	t_token	*command_first;
+	t_token	*cmd_first;
 
 	temp = first;
-	command_first = NULL;
+	cmd_first = NULL;
 	while (temp)
 	{
-		if (!command_first)
-			command_first = temp;
+		if (!cmd_first)
+			cmd_first = temp;
 		if (temp->maintoken == tkn_semi && temp->next)
-		{
-			command_first = run_semicolon_command(&command_first, term, temp);
-			temp = command_first;
-		}
+			callback_and_set(&temp, &cmd_first, term, &run_semicolon_command);
 		else if (temp->maintoken == tkn_semi && !temp->next && temp->prev)
 		{
 			temp->prev->next = NULL;
 			free_token(&temp);
 		}
 		else if (temp->maintoken == tkn_and_if)
-		{
-			command_first = run_and_if_command(&command_first, term, &temp);
-			temp = command_first;
-		}
+			callback_and_set(&temp, &cmd_first, term, &run_and_if_command);
 		else if (temp->maintoken == tkn_or_if)
-		{
-			command_first = run_or_if_command(&command_first, term, &temp);
-			temp = command_first;
-		}
+			callback_and_set(&temp, &cmd_first, term, &run_or_if_command);
 		else
 			temp = temp->next;
 	}
-	return (command_first);
+	return (cmd_first);
 }
